2d78fa5883def10452f95b7e7e93676d0d9e242b
Demeyer10 <04_zoster_borscht@icloud.com>
2024-10-19 15:41:30 -0400

    Stack implemented my way

---
+++ Medium/155-Min-Stack.py
@@ -0,0 +1,12 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        if self.stack:
+…        else:
+            return 0
