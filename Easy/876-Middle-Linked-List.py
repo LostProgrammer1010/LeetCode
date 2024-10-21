c71c8143aa761e2cac94b75948b8c4ea27992254
Demeyer10 <04_zoster_borscht@icloud.com>
2024-10-20 21:28:42 -0400

    Fast slow better implementation

--- Easy/876-Middle-Linked-List.py
+++ Easy/876-Middle-Linked-List.py
@@ -1,18 +1,9 @@
 def middleNode(head: Optional[ListNode]) -> Optional[ListNode]:
-
-    if head is None or head.next is None:
-        return head
-
-    if head.next.next is None:
-        return head.next
-
-    middle = head
     fast = head
-    while(fast):
-        if fast.next:
-            fast = fast.next.next
-            middle = middle.next
-        else:
-            break
+    slow = head
 
-    return middle
+    while(fast and fast.next):
+        fast = fast.next.next
+        slow = slow.next
+
+    return slow
